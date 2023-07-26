882bcfb556765b1c0c68f5ac71d47816a24661da
Leonardo Abreu <lcabreup@gmail.com>
2023-07-25 21:12:40 -0400

    Code updated.

--- cipher.py
+++ cipher.py
@@ -1,1 +1,15 @@
 # add your code here
+# Will do the caesar cipher converting uppercases to lowercases
+lowercases = "abcdefghijklmnopqrstuvwxyz"
+msgtoencrypt = input("Please enter a sentence:")
+msgtoencrypt = msgtoencrypt.lower()
+msgencrypted = ''
+for i in range(len(msgtoencrypt)):
+    if msgtoencrypt[i] not in lowercases:
+        msgencrypted += msgtoencrypt[i]
+    else:
+        msgencrypted += lowercases[(lowercases.index(msgtoencrypt[i]) + 5) % 26]
+print("The encrypted sentence is:", msgencrypted)
+
+
+
